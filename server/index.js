9e9e8708daca667e38e6a9c719e5753a420db924
shaik-baleeghuddin-kashif <hello@sbkashif.com>
2024-08-25 15:51:09 +0530

    Fixing Cors Issue

--- server/index.js
+++ server/index.js
@@ -12,8 +12,6 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(cors({origin: "http://localhost:5173", credentials: true}))
-app.use(cors({origin: "http://192.168.1.2:5173", credentials: true}))
-app.use(cors({origin: "http://192.168.240.1:5173", credentials: true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
