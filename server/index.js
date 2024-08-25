4b5772a8c35670d015476c27c9350d2eba23c130
shaik-baleeghuddin-kashif <hello@sbkashif.com>
2024-08-25 14:38:44 +0530

    Done with Backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js"
 import authRoutes from "./routes/auth.route.js"
@@ -10,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
-app.use("/api/auth", authRoutes)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     connectDB();
